588bde76ad3b84cc5f602348f6b84795b09447fc
Zafer Yilmaz <120649973+Zafer-Yilmaz@users.noreply.github.com>
2023-01-30 19:47:18 +0100

    task 1 yapildi zafer

--- src/Group1.java
+++ src/Group1.java
@@ -1,5 +1,7 @@
 public class Group1 {
     public static void main(String[] args) {
         System.out.println("Github Macerası");
+
+
     }
 }
---
+++ src/Zafer.java
@@ -0,0 +1,6 @@
+public class Zafer {
+    public static void main(String[] args) {
+
+        System.out.println("Zafer den merhaba");
+    }
+}
